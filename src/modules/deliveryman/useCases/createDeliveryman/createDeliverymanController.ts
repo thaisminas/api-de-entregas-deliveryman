7464edcc1036740b68ec1a42c45d1925baeed47d
thaisminas <thaisminasds@gmail.com>
2022-01-22 19:17:34 -0300

    adicionando o create deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/createDeliverymanController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateDeliverymanUseCase } from './createDeliverymanUseCase';
+
+
+
+export class CreateDeliverymanController {
+    async handle(request: Request, response: Response) {
+        const { username, password } = request.body;
+
+        const createDeliverymanUseCase = new CreateDeliverymanUseCase();
+        
+        const result = await createDeliverymanUseCase.execute({
+            username,
+            password,
+        });
+
+        return response.json(result);
+    }
+}
